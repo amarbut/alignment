2c5bfe095b5d0e04d352d42706ce703972ee2384
amarbut <anna.marbut@gmail.com>
2025-10-14 12:02:49 -0600

    debugging llama base refusal

--- pipeline/submodules/select_direction.py
+++ pipeline/submodules/select_direction.py
@@ -214,7 +214,7 @@ def select_direction(
     baseline_refusal_scores_harmful = get_refusal_scores(model_base.model, harmful_instructions, model_base.tokenize_instructions_fn, model_base.refusal_toks, phrase_ids, fwd_hooks=[], batch_size=batch_size)
     
     #print("baseline_refusal_scores_harmful:", baseline_refusal_scores_harmful)
-    baseline_refusal_scores_harmless = get_refusal_scores(model_base.model, harmless_instructions, model_base.tokenize_instructions_fn, model_base.refusal_toks, phrase_ids, fwd_hooks=[], batch_size=batch_size, print_response = True, tokenizer = model.tokenizer)
+    baseline_refusal_scores_harmless = get_refusal_scores(model_base.model, harmless_instructions, model_base.tokenize_instructions_fn, model_base.refusal_toks, phrase_ids, fwd_hooks=[], batch_size=batch_size, print_response = True, tokenizer = model_base.tokenizer)
 
     ablation_kl_div_scores = torch.zeros((n_pos, n_layer), device=model_base.model.device, dtype=torch.float64)
     ablation_refusal_scores = torch.zeros((n_pos, n_layer), device=model_base.model.device, dtype=torch.float64)
