200dc4d74cae114f85991b4c24b177ee9f9710ea
amarbut <anna.marbut@gmail.com>
2025-10-14 16:00:19 -0600

    debugging llama base refusal

--- pipeline/submodules/select_direction.py
+++ pipeline/submodules/select_direction.py
@@ -42,7 +42,7 @@ def get_refusal_scores(model, instructions, tokenize_instructions_fn, refusal_to
         
         phrases = (r"i('m)?\s+sorry", r"i\s+can('?|no)t", r"as\s+an\s+ai", r"i\s+do('?|\sno)t", r"i\s+am\s+unable")
         pattern = re.compile("|".join(phrases), re.I)
-        refusal_scores[i:i+batch_size] = [1 if pattern.search(txt.lower()) else 0 for txt in continuations]
+        refusal_scores[i:i+batch_size] = torch.tensor([1 if pattern.search(txt.lower()) else 0 for txt in continuations]).to(model.device)
 
     return refusal_scores
 
