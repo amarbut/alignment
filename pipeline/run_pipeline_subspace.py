9ee4685160dc37797048895de73495828720ad8e
amarbut <anna.marbut@gmail.com>
2025-11-08 22:18:33 -0700

    generalizing subspace pipeline

--- pipeline/run_pipeline_subspace.py
+++ pipeline/run_pipeline_subspace.py
@@ -23,6 +23,7 @@ def parse_arguments():
     parser.add_argument('--model_path', type=str, required=True, help='Path to the model')
     parser.add_argument('--skip_generate', type=bool, default=False)
     parser.add_argument('--method', type=str, default="arditi", help="direction/subspace pipeline to use", choices=["arditi", "cpca", "pls", "nonlinear", "arditi_auc"])
+    parser.add_argument('--topk', type=int, default=1, description="Number of components to include in subspace; topk=1 is a single vector")
     return parser.parse_args()
 
 def load_and_sample_datasets(cfg):
@@ -63,8 +64,42 @@ def filter_data(cfg, model_base, harmful_train, harmless_train, harmful_val, har
     
     return harmful_train, harmless_train, harmful_val, harmless_val
 
+def generate_and_save_candidate_directions(cfg, model_base, harmful_train, harmless_train):
+    """Generate and save candidate directions."""
+    if not os.path.exists(os.path.join(cfg.artifact_path(), 'generate_directions')):
+        os.makedirs(os.path.join(cfg.artifact_path(), 'generate_directions'))
 
-def generate_and_save_activations(model_base, harmless_train, harmful_train, cfg, batch_size: int = 32):
+    mean_diffs = generate_directions(
+        model_base,
+        harmful_train,
+        harmless_train,
+        artifact_dir=os.path.join(cfg.artifact_path(), "generate_directions"))
+
+    torch.save(mean_diffs, os.path.join(cfg.artifact_path(), 'generate_directions/mean_diffs.pt'))
+
+    return [mean_diffs]
+
+def select_and_save_direction(cfg, model_base, harmful_val, harmless_val, candidate_directions, topk):
+    """Select and save the direction."""
+    if not os.path.exists(os.path.join(cfg.artifact_path(), 'select_direction')):
+        os.makedirs(os.path.join(cfg.artifact_path(), 'select_direction'))
+
+    pos, layer, direction = select_direction(
+        model_base,
+        harmful_val,
+        harmless_val,
+        candidate_directions[0],
+        artifact_dir=os.path.join(cfg.artifact_path(), "select_direction")
+    )
+
+    with open(f'{cfg.artifact_path()}/direction_metadata.json', "w") as f:
+        json.dump({"pos": pos, "layer": layer}, f, indent=4)
+
+    torch.save(direction, f'{cfg.artifact_path()}/direction.pt')
+
+    return pos, layer, direction
+
+def generate_and_save_activations(cfg, model_base, harmless_train, harmful_train, batch_size: int = 32):
     
     if not os.path.exists(os.path.join(cfg.artifact_path(), 'generate_acts')):
         os.makedirs(os.path.join(cfg.artifact_path(), 'generate_acts'))
@@ -87,18 +122,19 @@ def generate_and_save_activations(model_base, harmless_train, harmful_train, cfg
     return hf_acts, hl_acts
 
 
-def select_and_save_cpca(cfg, model_base, harmful_acts, harmless_acts, topk = 3, keep_var = 0.999, eig_floor_frac = 1e-3):
+def select_and_save_cpca(cfg, model_base, harmful_val, harmless_val, cands, topk, keep_var = 0.999, eig_floor_frac = 1e-3):
     if not os.path.exists(os.path.join(cfg.artifact_path(), 'select_cpca')):
         os.makedirs(os.path.join(cfg.artifact_path(), 'select_cpca'))
 
-    layer, pos, res = choose_best_cpca(harmful_acts, harmless_acts, topk, keep_var, eig_floor_frac)
+    layer, pos, res = choose_best_cpca(cands[1], cands[0], topk, keep_var, eig_floor_frac)
 
     with open(f'{cfg.artifact_path()}/select_cpca/subspace_metadata.json', "w") as f:
         json.dump({"pos": pos, "layer": layer}, f, indent=4)
 
     torch.save(res, f'{cfg.artifact_path()}/select_cpca/subspace_res.pt')
+    torch.save(res["components"][:,:topk], f'{cfg.artifact_path()}/direction.pt')
 
-    return layer, pos, res
+    return layer, pos, res["components"][:,:topk]
 
 def generate_and_save_completions_for_dataset(cfg, model_base, fwd_pre_hooks, fwd_hooks, intervention_label, dataset_name, dataset=None):
     """Generate and save completions for a dataset."""
@@ -139,12 +175,25 @@ def evaluate_loss_for_datasets(cfg, model_base, fwd_pre_hooks, fwd_hooks, interv
     with open(f'{cfg.artifact_path()}/loss_evals/{intervention_label}_loss_eval.json', "w") as f:
         json.dump(loss_evals, f, indent=4)
 
-def run_pipeline(model_path, skip_generate, method):
+method_dict = {"arditi": {"candidate_loc": ["generate_directions/mean_diffs.pt"],
+                          "generate_cands": generate_and_save_candidate_directions,
+                          "select_dirs": select_and_save_direction
+                         },
+               "cpca": {"candidate_loc": ["generate_acts/hl_activations.pt", "generate_acts/hf_activations.pt"],
+                        "generate_cands": generate_and_save_activations,
+                        "select_dirs": select_and_save_cpca
+                       }
+              }
+    
+
+def run_pipeline(model_path, skip_generate, method, topk):
     """Run the full pipeline."""
-    model_alias = os.path.basename(model_path)+f"/method"
+    model_alias = os.path.basename(model_path)+f"/{method}"
     cfg = Config(model_alias=model_alias, model_path=model_path)
+    method_args = method_dict[method]
 
     model_base = construct_model_base(cfg.model_path)
+    model_base.model.config.pad_token_id = model_base.tokenizer.pad_token_id
 
     # Load and sample datasets
     harmful_train, harmless_train, harmful_val, harmless_val = load_and_sample_datasets(cfg)
@@ -156,19 +205,16 @@ def run_pipeline(model_path, skip_generate, method):
 
     # 1. Generate or load candidate refusal directions
     if skip_generate:
-        hl_acts = torch.load(os.path.join(cfg.artifact_path(), "generate_acts/hl_activations.pt"), map_location = "cpu")
-        hf_acts = torch.load(os.path.join(cfg.artifact_path(), "generate_acts/hf_activations.pt"), map_location = "cpu")
+        cands = [torch.load(os.path.join(cfg.artifact_path(), p), map_location = "cpu") for p in method_args["candidate_loc"]
     else:
-        hf_acts, hl_acts = generate_and_save_activations(model_base, harmless_train, harmful_train, cfg)
+        cands = method_args["generate_cands"](cfg, model_base, harmless_train, harmful_train)
     
-    # 2. Select the most effective refusal direction
-    # TODO: hard-coded settings
-    layer, pos, res = select_and_save_cpca(cfg, model_base, hf_acts, hl_acts, topk = 3, keep_var = 0.999, eig_floor_frac = 1e-3) 
+    # 2. Select the most effective refusal direction/subspace
+    layer, pos, direction = method_args["select_dirs"](cfg, model_base, harmful_val, harmless_val, cands, topk = topk) 
 
     baseline_fwd_pre_hooks, baseline_fwd_hooks = [], []
-    # TODO: # hard-coded top 3 components
-    ablation_fwd_pre_hooks, ablation_fwd_hooks = get_all_subspace_ablation_hooks(model_base, res["components"][:,:3]) 
-    actadd_fwd_pre_hooks, actadd_fwd_hooks = [(model_base.model_block_modules[layer], get_activation_addition_subspace_input_pre_hook(res["components"][:,:3], coeffs=torch.tensor(-1.0)))], []
+    ablation_fwd_pre_hooks, ablation_fwd_hooks = get_all_subspace_ablation_hooks(model_base, direction) 
+    actadd_fwd_pre_hooks, actadd_fwd_hooks = [(model_base.model_block_modules[layer], get_activation_addition_subspace_input_pre_hook(direction, coeffs=torch.tensor(-1.0)))], []
 
     print("Generate and save completions on harmful evaluation datasets")
     for dataset_name in cfg.evaluation_datasets:
@@ -186,8 +232,7 @@ def run_pipeline(model_path, skip_generate, method):
     harmless_test = random.sample(load_dataset_split(harmtype='harmless', split='test'), cfg.n_test)
 
     generate_and_save_completions_for_dataset(cfg, model_base, baseline_fwd_pre_hooks, baseline_fwd_hooks, 'baseline', 'harmless', dataset=harmless_test)
-    
-    actadd_refusal_pre_hooks, actadd_refusal_hooks = [(model_base.model_block_modules[layer], get_activation_addition_subspace_input_pre_hook(res["components"][:,:3], coeffs=torch.tensor(+1.0)))], []
+    actadd_refusal_pre_hooks, actadd_refusal_hooks = [(model_base.model_block_modules[layer], get_activation_addition_subspace_input_pre_hook(direction, coeffs=torch.tensor(+1.0)))], []
     generate_and_save_completions_for_dataset(cfg, model_base, actadd_refusal_pre_hooks, actadd_refusal_hooks, 'actadd', 'harmless', dataset=harmless_test)
 
     print("Evaluate completions and save results on harmless evaluation dataset")
@@ -201,4 +246,4 @@ def run_pipeline(model_path, skip_generate, method):
 
 if __name__ == "__main__":
     args = parse_arguments()
-    run_pipeline(model_path=args.model_path, skip_generate=args.skip_generate, method=args.method)
+    run_pipeline(model_path=args.model_path, skip_generate=args.skip_generate, method=args.method, topk=args.topk)
